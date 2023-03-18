5cd21ccab5065cae4d72f9ff9d4a6c49cc6b5852
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-03-18 14:20:54 -0400

    retrieve deck names and display them

--- src/pages/decks.tsx
+++ src/pages/decks.tsx
@@ -1,10 +1,33 @@
 import React, { useState }from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { signOut, useSession } from 'next-auth/react';
+import { signOut, useSession, getSession } from 'next-auth/react';
 import Router from 'next/router';
+import { GetServerSideProps } from 'next';
+import { InferGetServerSidePropsType } from 'next'
+import prisma from '../../prisma/lib/prisma'
 
- const Decks: React.FC = () => {
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+    const session = await getSession({ req });
+    if (!session) {
+      res.statusCode = 403;
+      return { props: { decks: [] } };
+    }
+  
+    const decks = await prisma.deck.findMany({
+      where: {
+        user: { email: session?.user?.email },
+      },
+      select: { name: true },
+      
+    });
+    return {
+      props: { decks },
+    };
+  };
+  
+ 
+ const Decks: React.FC  = ({ decks }: InferGetServerSidePropsType <typeof getServerSideProps>) => {
     const [newDeck,setNewDeck] = useState<boolean>(false)
     const [title,setTitle] = useState<string>('')
     const handleCreateDeck = async (e: React.SyntheticEvent) => {
@@ -21,6 +44,7 @@ import Router from 'next/router';
                 console.log(error)
             }
     };
+    console.log(decks);
     return(
         <>
             <h3 onClick={()=> setNewDeck(!newDeck)} >New Deck</h3>
@@ -31,6 +55,16 @@ import Router from 'next/router';
             </label>
             }
             <button onClick={handleCreateDeck}>New Deck</button>
+            <div className="decks">
+                {decks.map((deck:any)=> {
+                    return(
+                        <div className="deck">
+                            <p>{deck.name}</p>
+                        </div>
+                    )
+                       
+                })}
+            </div>
         </>
     )
  }
