39860a73650ba4b0e2ac70ffe1ecaf41c34847cc
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-04-08 16:12:00 -0400

    added next_public in front of api key for access

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
  const[loggedIn, setLoggedIn] = useState<boolean>(false);
   //openAI
   const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
   
