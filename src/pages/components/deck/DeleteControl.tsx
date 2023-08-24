856a673675e6b53c9417adc7c561b01104957ef1
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-08-24 09:53:13 -0500

    update decks stored in context when one is deleted from database

--- src/pages/components/deck/DeleteControl.tsx
+++ src/pages/components/deck/DeleteControl.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Image from "next/image";
 import router from "next/router";
+import { useDecks } from "@/pages/context/decks-context";
+import prisma from "../../../../prisma/lib/prisma";
+import { useSession } from "next-auth/react";
 type Props = {
   deckId: string;
 };
+
 export default function DeleteControl(props: Props): JSX.Element {
   const { deckId } = props;
+  const { decks, updateDecks } = useDecks();
   const handleDelete = async (e: React.SyntheticEvent, deckId: string) => {
     e.preventDefault();
     fetch("/api/deck/delete?id=" + deckId, {
@@ -13,8 +18,9 @@ export default function DeleteControl(props: Props): JSX.Element {
       headers: { "Content-Type": "application/json" },
     })
       .then(() => {
-        //refresh to get server side props
-        router.replace(router.asPath);
+        //filter the deckid out of decks array, so context can update
+        const updatedDecks = decks.filter((deck) => deck.id !== deckId);
+        updateDecks(updatedDecks);
       })
       .catch((error) => {
         console.log("error", error);
