58a8f6c08f8ac3a3ade906757a48173ae8b1a0f2
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-08-25 13:42:03 -0500

    return to homescreen on logout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, use } from "react";
 import Header from "../components/Header";
 import { WordOfDayContainer } from "../components/wordOfDay";
 import { TranslateWrapper } from "../components/translate";
 import { WritingWrapper } from "../components/write";
 import { TranslationData } from "../components/translate/types.translate";
-
+import { useSession } from "next-auth/react";
+import router from "next/router";
 const initialTranslation: TranslationData = {
   toTranslate: "",
   translation: "",
@@ -14,11 +15,16 @@ const initialTranslation: TranslationData = {
 };
 
 export default function Home() {
+  const session = useSession();
   const [translationData, setTranslationData] =
     useState<TranslationData>(initialTranslation);
 
   /*   const [loggedIn, setLoggedIn] = useState<boolean>(false); */
-
+  useEffect(() => {
+    if (session.status === "authenticated") {
+      router.push("/");
+    }
+  }, [session]);
   return (
     <div className="homepage">
       <Header />
