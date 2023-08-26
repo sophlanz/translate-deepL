f7732d77d0d2895e289a824ff12c48e88a1c48ce
Sophia Lanzendorf <sophialanzendorf@gmail.com>
2023-08-26 15:45:20 -0500

    DeleteControl component status monitoring, and display ErrorMessage component if error

--- src/components/card/DeleteControl.tsx
+++ src/components/card/DeleteControl.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { useCards } from "../../context/card-context";
-
+import ErrorMessage from "../errors/ErrorMessage";
+enum Status {
+  idle,
+  Loading,
+  Error,
+}
 export default function DeleteControl(props: { cardId: string }): JSX.Element {
   const { cardId } = props;
   const { cards, updateCards } = useCards();
+  const [status, setStatus] = useState<Status>(Status.idle);
+  const [error, setError] = useState<Error | undefined>();
   const handleDelete = async (e: React.SyntheticEvent, cardId: string) => {
     e.preventDefault();
-
+    setStatus(Status.Loading);
     await fetch("/api/card/delete?id=" + cardId, {
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
@@ -16,10 +23,16 @@ export default function DeleteControl(props: { cardId: string }): JSX.Element {
         //update context
         const updatedCards = cards.filter((card) => card.id !== cardId);
         updateCards(updatedCards);
+        setStatus(Status.idle);
       })
       .catch((error) => {
         console.log(error);
+        setStatus(Status.Error);
+        setError(error);
       });
+    if (status === Status.Error) {
+      <ErrorMessage error={error} />;
+    }
   };
   return (
     <div onClick={(e) => handleDelete(e, cardId)}>
